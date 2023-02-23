8453052c86388212c7522bac19474bd094f81fb7
ChrisJP84 <christopher.jprice@outlook.com>
2023-02-23 12:58:21 +0000

    restructuring component directories

--- midiSequencerInputUnit/Firmware/components/system/include/systemEvents.h
+++
@@ -1,6 +0,0 @@
-#include <stdbool.h>
-#include <stdio.h>
-
-
-uint8_t createNewSequencerProject(uint8_t * midiFileBufferPtr, char * fileName, uint8_t tempo, uint8_t quantization);
-uint8_t printEntireMidiFileContentToConsole(uint8_t * fileBASE, uint32_t fileSize);
